47277357f308ffb45a2d72478aeecd3099315ae2
Coding-Container <blgnd227@gmail.com>
2026-07-17 10:22:17 +0530

    Migrated email service from Gmail SMTP to Resend

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -3,12 +3,19 @@ const { Resend } = require("resend");
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async (email, subject, html) => {
-  await resend.emails.send({
-    from: "MyEcomS <onboarding@resend.dev>",
-    to: email,
-    subject,
-    html,
-  });
+  try {
+    const data = await resend.emails.send({
+      from: "MyEcomS <onboarding@resend.dev>",
+      to: email,
+      subject,
+      html,
+    });
+
+    console.log("Email Sent:", data);
+  } catch (err) {
+    console.error("Resend Error:", err);
+    throw err;
+  }
 };
 
 module.exports = sendEmail;
